d173a2bbf9ea7417de95f8ac65937222db9b5e19
Adi Shanbhag <adishan8126@gmail.com>
2025-11-26 15:03:30 -0800

    refactor: rename DataVisualizationWithCode to InputCode and update all imports/routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
 import ButtonDemo from './pages/ButtonDemo'
+import DataVisualizationRuleBuilder from './pages/DataVisualizationRuleBuilder'
+import InputCodePage from './pages/InputCode'
+import FilePickerPage from './pages/FilePickerPage'
 
 export default function App() {
   return (
-    <div className="app">
-      <ButtonDemo />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <nav style={{marginBottom: 24, display: 'flex', gap: 18, fontSize: 16}}>
+          <Link to="/">Home</Link>
+          <Link to="/button-demo">Button Demo</Link>
+          <Link to="/data-visualization-rule-builder">Data Visualization Rule Builder</Link>
+          <Link to="/input-code">Input Code Demo</Link>
+          <Link to="/file-picker">File Picker Demo</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/button-demo" element={<ButtonDemo />} />
+          <Route path="/data-visualization-rule-builder" element={<DataVisualizationRuleBuilder />} />
+          <Route path="/input-code" element={<InputCodePage />} />
+          <Route path="/file-picker" element={<FilePickerPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/InputCode.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import DataVisualizationRuleBuilderRow from '../components/DataVisualizationRuleBuilderRow'
+import InputCode from '../components/InputCode'
+
+const sampleSeries = [
+  { name: 'Series 1', color: 'var(--dvt-1)', values: [45, 46, 47, 46] },
+  { name: 'Series 2', color: 'var(--dvt-2)', values: [30, 31, 30, 31] },
+  { name: 'Series 3', color: 'var(--dvt-3)', values: [20, 20, 20, 20] },
+  { name: 'Series 4', color: 'var(--dvt-4)', values: [40, 41, 40, 41] }
+]
+
+const labels = ['Q1', 'Q2', 'Q3', 'Q4']
+
+export default function InputCodePage() {
+  return (
+    <div style={{ padding: 16 }}>
+      <div style={{ marginBottom: 18 }}>
+        <DataVisualizationRuleBuilderRow order={1} fieldLabel="Resource" fieldValue="Instance" series={sampleSeries} labels={labels} onRemove={null} />
+      </div>
+
+      <div style={{ marginTop: 24 }}>
+        <InputCode
+          label="Policy"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+          learnMoreText="Learn more"
+          copyAction
+          lines={6}
+          codeText={'Allow group <group-name> to read buckets in compartment <compartment-name>'}
+        />
+      </div>
+    </div>
+  )
+}
